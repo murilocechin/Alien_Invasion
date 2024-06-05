bd1ca4ac4c5ca6e3004d4ebd93e25e6e5052af86
Murilo Cechin <murilo.cechin@gmail.com>
2024-06-05 15:12:26 -0300

    Rounding the Score

--- scoreboard.py
+++ scoreboard.py
@@ -25,7 +25,8 @@ class Scoreboard:
         """
         Turn the score into a rendered image
         """
-        score_str = str(self.stats.score)
+        rounded_score = round(self.stats.score, -1)
+        score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
 
         # Display the score at the top right of the screen
